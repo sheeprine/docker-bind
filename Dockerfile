4fc0eb539a643115075e62c3832acbfa23872428
Stéphane Albert <sheeprine@oh.its.fake.nullplace.com>
2017-06-01 17:13:23 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM alpine:latest
+MAINTAINER Stephane Albert "sheeprine@nullplace.com"
+
+ENV RNDC_KEY_FILE="/run/secrets/rndc.key"
+RUN apk add --no-cache bind && rm -rf /var/cache/apk/* && \
+    # ln -s /dev/null /etc/bind/rndc.key && \
+    # touch /etc/bind/named.conf && \
+    chown named: /etc/bind
+
+COPY entrypoint.sh /
+COPY /templates /templates
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env sh
+set -x
+
+CONFIG_DIR="/etc/bind"
+CONFIG_FILE="${CONFIG_DIR}/named.conf"
+ZONE_MASTER_TEMPLATE="/templates/zone.master.conf.template"
+ZONE_SLAVE_TEMPLATE="/templates/zone.slave.conf.template"
+
+XFER_IP=${XFER_IP:-}
+TRUSTED_IP=${TRUSTED_IP:-127.0.0.0/8 ::1/128}
+MASTERS_IP=${MASTERS_IP:-}
+FORWARDERS=${FORWARDERS:-8.8.4.4 8.8.8.8 2001:4860:4860::8888 2001:4860:4860::8844}
+
+# Zones definition
+MASTER_ZONES=${MASTER_ZONES:-}
+SLAVE_ZONES=${SLAVE_ZONES:-}
+
+# Security
+RNDC_KEY_LINK="/etc/bind/rndc.key"
+RNDC_KEY_FILE=${RNDC_KEY_FILE:-$RNDC_KEY_LINK}
+
+check_or_create_rndc() {
+    if [ ! -f $RNDC_KEY_FILE ]; then
+        rndc-confgen -a
+        RNDC_KEY_FILE=$RNDC_KEY_LINK
+    else
+        ln -s $RNDC_KEY_FILE $RNDC_KEY_LINK
+    fi
+}
+
+format_ips() {
+    for ip in $@; do
+        echo -n "$ip;\n"
+    done
+}
+
+create_list() {
+    local list_type=$1
+    shift
+    local list_name=$1
+    shift
+    local list_ips=$@
+    echo "$list_type \"$list_name\" {"
+    echo -e "$(format_ips $list_ips)};"
+}
+
+create_zone() {
+    local zone_type=${2:-"master"}
+    if [ $zone_type = "slave" ]; then
+        local zone_folder="sec"
+        local zone_template=$ZONE_SLAVE_TEMPLATE
+    else
+        local zone_folder="pri"
+        local zone_template=$ZONE_MASTER_TEMPLATE
+    fi
+    sed "s~@ZONE@~$1~g" $zone_template | \
+    sed "s~@TYPE@~$zone_type~g" | \
+    sed "s~@FOLDER@~$zone_folder~g" >>${CONFIG_FILE}
+}
+
+create_main_config() {
+    sed "s~@FORWARDERS@~$(format_ips $FORWARDERS)~g" \
+        /templates/named.conf.template | \
+    sed "s~@RNDC_KEY_FILE@~$RNDC_KEY_FILE~g" >>${CONFIG_FILE}
+}
+
+rm $CONFIG_FILE
+check_or_create_rndc
+
+create_list "acl" "xfer" $XFER_IP >>${CONFIG_FILE}
+create_list "acl" "trusted" $TRUSTED_IP >>${CONFIG_FILE}
+create_list "acl" "masters" $MASTERS_IP >>${CONFIG_FILE}
+create_list "masters" "masters" $MASTERS_IP >>${CONFIG_FILE}
+
+create_main_config
+
+for zone in $MASTER_ZONES; do
+    create_zone $zone master
+done
+
+for zone in $SLAVE_ZONES; do
+    create_zone $zone slave
+done
+
+VERSION=$(named -v | cut -d' ' -f2)
+echo "Starting bind $VERSION..."
+exec named -g -u named $@
